b308bf1ba92ab9491a34721da0e4cefdeafc2e6d
Theo Billet <billettheo5@gmail.com>
2023-01-13 17:07:22 +0100

    Part 3: file->amqp

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/GameModel.java
@@ -0,0 +1,14 @@
+package fr.lernejo.fileinjector;
+
+public record GameModel(int id,
+                        String title,
+                        String thumbnail,
+                        String short_description,
+                        String game_url,
+                        String genre,
+                        String platform,
+                        String publisher,
+                        String developer,
+                        String release_date,
+                        String freetogame_profile_url) {
+}
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,37 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            return;
+        }
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
-            System.out.println("Hello after starting Spring");
+            ArrayList<GameModel> games = new ArrayList<>();
+            games = (new ObjectMapper()).readValue(Paths.get(args[0]).toFile(), new TypeReference<ArrayList<GameModel>>(){});
+            var rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+            for (GameModel game : games) {
+                rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+                rabbitTemplate.convertAndSend("", "game_info", game, m -> {
+                    m.getMessageProperties().getHeaders().put("game_id", game.id());
+                    return m;
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
